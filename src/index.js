76ad72fed1b05219c64905784824fbe2b6424379
NN Fathoni <naufalfathoni79@gmail.com>
2021-12-10 11:03:54 +0700

    Rewrite Board to use two loops

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
+        key={i}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
@@ -23,21 +24,13 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {[...Array(3)].map((x, i) => {
+          return (
+            <div key={i} className="board-row">
+              {[...Array(3)].map((y, j) => this.renderSquare(j + 3*i))}
+            </div>
+          );
+        })}
       </div>
     );
   }
