50328f0c9a7d3af67cf0ddfa721e8cb271cfd4bb
NN Fathoni <naufalfathoni79@gmail.com>
2021-12-10 17:56:19 +0700

    Display draw message

--- src/index.js
+++ src/index.js
@@ -129,8 +129,10 @@ class Game extends React.Component {
     let status;
     if (winner) {
       status = 'Winner: ' + winner.player;
-    } else {
+    } else if (this.state.stepNumber < 9) {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    } else {
+      status = 'Draw!';
     }
 
     return (
