99373842caee459dfd051610698ecad68c16bd47
NN Fathoni <naufalfathoni79@gmail.com>
2021-12-10 10:15:01 +0700

    Bold current move

--- src/index.js
+++ src/index.js
@@ -99,7 +99,12 @@ class Game extends React.Component {
         'Go to game start';
       return (
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          <button
+            className={move === this.state.stepNumber ? 'bold' : ''}
+            onClick={() => this.jumpTo(move)}
+          >
+            {desc}
+          </button>
         </li>
       );
     });
